0c175a8d30d14fe805371970fac3b176650c1039
Ashish <94269501+ashishdock@users.noreply.github.com>
2021-12-09 12:53:31 +0530

    Uploading XML practice codes
    Uploading XML practice codes to GitHub. :anchor: :dolphin:

---
+++ XML/events.js
@@ -0,0 +1,6 @@
+function setBtn(obj,state) {
+	if(state==1) obj.setAttribute("stroke","red");
+	else if(state==2) obj.setAttribute("stroke","green");
+	else if(state==3) obj.setAttribute("stroke","blue");
+	else if(state==4) obj.setAttribute("stroke","none");
+}
