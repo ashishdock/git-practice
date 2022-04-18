a6716985fcd3a7f207f6ccfb2dd5737f28d4e495
Ashish <94269501+ashishdock@users.noreply.github.com>
2022-04-18 12:23:49 +0530

    Java daily :squirrel:

---
+++ Java/Java-TCR/TextAreaDemo.java
@@ -0,0 +1,34 @@
+// Demonstrate TextArea.
+import java.awt.*;
+import java.awt.event.*;
+
+public class TextAreaDemo extends Frame
+{
+	public TextAreaDemo()
+	{
+		// Use a flow layout
+		setLayout(new FlowLayout());
+		
+		String val = "Java 9 is the latest version of the most \n" + "widely-used computer language for Internet programming.\n" + "Building on a rich heritage, Java has advanced both\n" + "the art and science of computer language design.\n\n" + "One of the reasons for Java's ongoing success is its\n" + "constant, steady rate of evolution. Javas has never stood\n" + "still. Instead, Java has consistently adapted to the\n" + "rapidly changing landscape of the networked world.\n" + "Moreover, Java has often led the way, charting the\n" + "course for others to follow.\n";
+		
+		TextArea text = new TextArea(val, 10, 30);
+		add(text);
+		
+		addWindowListener(new WindowAdapter()
+		{
+			public void windowClosing(WindowEvent we)
+			{
+				System.exit(0);
+			}
+		});
+	}
+	
+	public static void main(String args[])
+	{
+		TextAreaDemo appwin = new TextAreaDemo();
+		
+		appwin.setSize(new Dimension(500, 500));
+		appwin.setTitle("Text Area Demo");
+		appwin.setVisible(true);
+	}
+}
